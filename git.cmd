534bec718a4377665c1b42874a9b1d0a98893322
Roman Tatár <roman.tatar@live.com>
2021-05-08 18:26:35 +0200

    Moves prompt update to gprompt.cmd

--- git.cmd
+++ git.cmd
@@ -1,14 +1,3 @@
 @echo off
 git.exe %*
-set GitError=%ErrorLevel%
-set GitBranch=
-IF %GitError% EQU 0 (
-    FOR /F %%I IN ('git.exe branch --show-current') DO set GitBranch=%%I
-)
-
-IF "%GitBranch%" == "" (
-    prompt
-) ELSE (
-    prompt $E[1;37m$P $E[32m[%GitBranch%]$E[37m $G$E[0;37m
-)
-EXIT /B %GitError%
+CALL gprompt.cmd
