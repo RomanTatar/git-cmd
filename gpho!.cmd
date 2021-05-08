da38262d2a3f59dd9c7d68e485f496de936c6e7e
Roman Tatár <roman.tatar@live.com>
2021-05-08 17:54:55 +0200

    Adds shortcuts for git push origin, git push origin --force

---
+++ gpho!.cmd
@@ -0,0 +1,11 @@
+@echo off
+IF NOT "%*" == "" (
+    set GitPushBranch=%*
+) ELSE (
+    IF NOT "%GitBranch%" == "" (
+        set GitPushBranch=%GitBranch%
+    ) ELSE (
+        set GitPushBranch=
+    )
+)
+git.exe push origin %GitPushBranch% --force
